8d1f7db148f5d9c43228de0aa0dc76fb994e1833
Michiel Schram <mdschram@hotmail.com>
2018-06-22 16:10:00 +0200

    working buttons

--- main.js
+++ main.js
@@ -1,23 +1,79 @@
 var main = new Vue({
     el: '#main',
     data: {
-        games: [],
-        page: 0
+        gameData: [],
+        pageShow: "games",
+        locations: [],
+        showLocation: "",
+        teams: [],
+        selectedData: {
+            "games": []
+        }
     },
     created: function () {
         this.fetch();
     },
     methods: {
         fetch: function () {
-            fetch('https://api.myjson.com/bins/9gjq2')
+            fetch('https://api.myjson.com/bins/pu872')
                 .then(r => r.json())
                 .then(json => {
-                    main.games = json
+                    this.gameData = json
+                    this.locationArray()
+                    this.teamsArray()
+                    this.selectedData = this.gameData
                 })
+        },
+        pageToggler: function (buttonName) {
+            if (this.locations.includes(buttonName)) {
+                this.selectedData = {
+                    "games": []
+                };
+                this.pageShow = "location";
+                this.showLocation = buttonName
+                for (i = 0; i < this.gameData.games.length; i++) {
+                    if (this.gameData.games[i].location == this.showLocation) {
+                        this.selectedData.games.push(this.gameData.games[i])
+                    }
+                }
+//                console.log(buttonName)
+//                console.log(this.pageShow)
+//                console.log(this.selectedData)
+            }
+            else if (this.teams.includes(buttonName)) {
+                this.selectedData = {
+                    "games": []
+                }
+                this.pageShow = "team";
+                this.showTeam = buttonName
+                for (i = 0; i < this.gameData.games.length; i++) {
+                    if (this.gameData.games[i].team1 == this.showTeam || this.gameData.games[i].team2 == this.showTeam) {
+                        this.selectedData.games.push(this.gameData.games[i])
+                    }
+                }
+            }
+            else {this.selectedData = this.gameData;
+//                  console.log(this.selectedData)
+                  console.log("not in array")
+//                  console.log(buttonName)
+//                  console.log(this.locations)/
+                  console.log(this.pageShow)
+                this.showPage = buttonName}
+           
+        },
+        locationArray: function () {
+            for (i = 0; i < this.gameData.games.length; i++) {
+                if (!this.locations.includes(this.gameData.games[i].location)) {
+                    this.locations.push(this.gameData.games[i].location)
+                }
+            }
+        },
+        teamsArray: function () {
+            for (i = 0; i < this.gameData.games.length; i++) {
+                if (!this.teams.includes(this.gameData.games[i].team1)) {
+                    this.teams.push(this.gameData.games[i].team1)
+                }
+            }
         }
-    },
-    pager: function(){
-        console.log(this.page)
     }
-
 })
